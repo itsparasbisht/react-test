4a4db68439ffb613be3fd975d7d555a443e30ae4
itsparasbisht <iamparasbisht@gmail.com>
2022-12-03 14:05:48 +0530

    __tests__ dir

--- src/components/greet/__test__/greet-test.jsx
+++ src/components/greet/__test__/greet-test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { Greet } from "./Greet";
+import { Greet } from "../Greet";
 
 describe("Greet", () => {
   test("renders correctly", () => {
