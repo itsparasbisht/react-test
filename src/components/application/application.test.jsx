2658dea678f8ff7bb0981953a7f38be6a5588fd6
itsparasbisht <iamparasbisht@gmail.com>
2022-12-13 20:18:31 +0530

    options to getByRole

--- src/components/application/application.test.jsx
+++ src/components/application/application.test.jsx
@@ -4,8 +4,16 @@ import Application from "./Application";
 describe("Application", () => {
   test("renders correctly", () => {
     render(<Application />);
-    const nameElement = screen.getByRole("textbox");
+    const nameElement = screen.getByRole("textbox", {
+      name: "Name",
+    });
     expect(nameElement).toBeInTheDocument();
+
+    const headingElement = screen.getByRole("heading", {
+      name: "Apply for role",
+      level: 1,
+    });
+    expect(headingElement).toBeInTheDocument();
 
     const jobLocationElement = screen.getByRole("combobox");
     expect(jobLocationElement).toBeInTheDocument();
