2bcf18e4b61b2f404b541d587deee70223c354eb
itsparasbisht <iamparasbisht@gmail.com>
2022-12-14 20:12:19 +0530

    testing against props

---
+++ src/components/skills/skills.test.jsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import Skills from "./Skills";
+
+describe("Skills Component", () => {
+  const skills = ["javascript", "git", "tailwind"];
+
+  test("renders correctly", () => {
+    render(<Skills skills={skills} />);
+    const listElement = screen.getByRole("list");
+    expect(listElement).toBeInTheDocument();
+  });
+
+  test("renders list items", () => {
+    render(<Skills skills={skills} />);
+    const listItemElements = screen.getAllByRole("listitem");
+    expect(listItemElements).toHaveLength(skills.length);
+  });
+});
