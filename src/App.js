5208ad779b27f5da41c23817505aa9c158f78853
itsparasbisht <iamparasbisht@gmail.com>
2022-12-16 19:40:35 +0530

    test click event

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import Application from "./components/application/Application";
+import Counter from "./components/counter/Counter";
 import Skills from "./components/skills/Skills";
 
 function App() {
   return (
     <div className="App">
-      <Skills skills={["JavaScript", "HTML", "CSS"]} />
+      <Counter />
     </div>
   );
 }
---
+++ src/components/counter/counter.test.jsx
@@ -0,0 +1,41 @@
+import { render, screen } from "@testing-library/react";
+import Counter from "./Counter";
+import user from "@testing-library/user-event";
+
+describe("Counter component", () => {
+  test("renders correctly", () => {
+    render(<Counter />);
+    const countElement = screen.getByRole("heading");
+    expect(countElement).toBeInTheDocument();
+
+    const incrementButton = screen.getByRole("button", { name: "Increment" });
+    expect(incrementButton).toBeInTheDocument();
+  });
+
+  test("renders a count of 0", () => {
+    render(<Counter />);
+    const countElement = screen.getByRole("heading");
+    expect(countElement).toHaveTextContent("0");
+  });
+
+  test("renders a count of 1 after clicking the increment button", async () => {
+    user.setup();
+    render(<Counter />);
+    const incrementButton = screen.getByRole("button", { name: "Increment" });
+    await user.click(incrementButton);
+
+    const countElement = screen.getByRole("heading");
+    expect(countElement).toHaveTextContent("1");
+  });
+
+  test("renders a count of 2 after clicking the increment button twice", async () => {
+    user.setup();
+    render(<Counter />);
+    const incrementButton = screen.getByRole("button", { name: "Increment" });
+    await user.click(incrementButton);
+    await user.click(incrementButton);
+
+    const countElement = screen.getByRole("heading");
+    expect(countElement).toHaveTextContent("2");
+  });
+});
