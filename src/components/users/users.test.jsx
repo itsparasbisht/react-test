e86a01207d8e36f2190e32508421c4f67a8af09d
itsparasbisht <iamparasbisht@gmail.com>
2022-12-19 20:13:13 +0530

    mock error handling

--- src/components/users/users.test.jsx
+++ src/components/users/users.test.jsx
@@ -1,4 +1,6 @@
 import { render, screen } from "@testing-library/react";
+import { rest } from "msw";
+import { server } from "../../mocks/server";
 import { Users } from "./Users";
 
 describe("Users", () => {
@@ -6,5 +8,20 @@ describe("Users", () => {
     render(<Users />);
     const users = await screen.findAllByRole("listitem");
     expect(users).toHaveLength(3);
+  });
+
+  test("renders error", async () => {
+    server.use(
+      rest.get(
+        "https://jsonplaceholder.typicode.com/users",
+        (req, res, ctx) => {
+          return res(ctx.status(500));
+        }
+      )
+    );
+
+    render(<Users />);
+    const error = await screen.findByText("Error fetching users");
+    expect(error).toBeInTheDocument();
   });
 });
